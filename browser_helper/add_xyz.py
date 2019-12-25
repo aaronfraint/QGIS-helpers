bb2acf6c78ad419b438c6019600cb2b32ee5d620
aaronfraint <38364429+aaronfraint@users.noreply.github.com>
2019-12-24 17:14:03 -0800

    initial commit

---
+++ browser_helper/add_xyz.py
@@ -0,0 +1,87 @@
+"""
+Summary of ``add_xyz.py``
+-------------------------
+
+Use the sources defined below to add entries under XYZ within the QGIS Browser
+
+"""
+
+# format:
+# display name, authcfg, pw, referer, url, username, zmax, zmin
+
+google_sources = [
+    ["Google Maps","","","","https://mt1.google.com/vt/lyrs=m&x=%7Bx%7D&y=%7By%7D&z=%7Bz%7D","","19","0"],
+    ["Google Satellite", "", "", "", "https://mt1.google.com/vt/lyrs=s&x=%7Bx%7D&y=%7By%7D&z=%7Bz%7D", "", "19", "0"],
+    ["Google Terrain", "", "", "", "https://mt1.google.com/vt/lyrs=t&x=%7Bx%7D&y=%7By%7D&z=%7Bz%7D", "", "19", "0"],
+    ["Google Terrain Hybrid", "", "", "", "https://mt1.google.com/vt/lyrs=p&x=%7Bx%7D&y=%7By%7D&z=%7Bz%7D", "", "19", "0"],
+    ["Google Satellite Hybrid", "", "", "", "https://mt1.google.com/vt/lyrs=y&x=%7Bx%7D&y=%7By%7D&z=%7Bz%7D", "", "19", "0"],
+]
+
+stamen_sources = [
+    ["Stamen Toner", "", "", "Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL", "http://tile.stamen.com/toner/%7Bz%7D/%7Bx%7D/%7By%7D.png", "", "20", "0"],
+    ["Stamen Toner Light", "", "", "Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL", "http://tile.stamen.com/toner-lite/%7Bz%7D/%7Bx%7D/%7By%7D.png", "", "20", "0"],
+    ["Stamen Watercolor", "", "", "Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL", "http://tile.stamen.com/watercolor/%7Bz%7D/%7Bx%7D/%7By%7D.jpg", "", "18", "0"],
+    ["Stamen Terrain", "", "", "Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL", "http://tile.stamen.com/terrain/%7Bz%7D/%7Bx%7D/%7By%7D.png", "", "20", "0"],
+]
+
+esri_sources = [
+    ["Esri Boundaries Places", "", "", "", "https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/%7Bz%7D/%7By%7D/%7Bx%7D", "", "20", "0"],
+    ["Esri Gray (dark)", "", "", "", "http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/%7Bz%7D/%7By%7D/%7Bx%7D", "", "16", "0"],
+    ["Esri Gray (light)", "", "", "", "http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/%7Bz%7D/%7By%7D/%7Bx%7D", "", "16", "0"],
+    ["Esri National Geographic", "", "", "", "http://services.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/%7Bz%7D/%7By%7D/%7Bx%7D", "", "12", "0"],
+    ["Esri Ocean", "", "", "", "https://services.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/%7Bz%7D/%7By%7D/%7Bx%7D", "", "10", "0"],
+    ["Esri Satellite", "", "", "", "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/%7Bz%7D/%7By%7D/%7Bx%7D", "", "17", "0"],
+    ["Esri Standard", "", "", "", "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/%7Bz%7D/%7By%7D/%7Bx%7D", "", "17", "0"],
+    ["Esri Terrain", "", "", "", "https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/%7Bz%7D/%7By%7D/%7Bx%7D", "", "13", "0"],
+    ["Esri Transportation", "", "", "", "https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer/tile/%7Bz%7D/%7By%7D/%7Bx%7D", "", "20", "0"],
+    ["Esri Topo World", "", "", "", "http://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/%7Bz%7D/%7By%7D/%7Bx%7D", "", "20", "0"],
+]
+
+osm_sources = [
+    ["OpenStreetMap Standard", "", "", "OpenStreetMap contributors, CC-BY-SA", "http://tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png", "", "19", "0"],
+    ["OpenStreetMap H.O.T.", "", "", "OpenStreetMap contributors, CC-BY-SA", "http://tile.openstreetmap.fr/hot/%7Bz%7D/%7Bx%7D/%7By%7D.png", "", "19", "0"],
+    ["OpenStreetMap Monochrome", "", "", "OpenStreetMap contributors, CC-BY-SA", "http://tiles.wmflabs.org/bw-mapnik/%7Bz%7D/%7Bx%7D/%7By%7D.png", "", "19", "0"],
+]
+
+SOURCES = google_sources + stamen_sources + esri_sources + osm_sources
+
+
+def remove_xyz_connections():
+    """ Remove all XYZ entries in the Browser """
+    qs = QSettings()
+
+    for k in sorted(qs.allKeys()):
+        if "qgis/connections-xyz/" in k:
+            qs.remove(k)
+
+    iface.reloadConnections()
+
+
+def add_xyz_connections(
+        extra_sources: list = [],
+        remove_existing: bool = True,
+) -> None:
+    """
+    Add XYZ connection for all default and user-specified sources
+
+    :param extra_sources: user-provided list to extend the stock choices
+    :param remove_existing: if True, will clear out old connections before adding new ones
+    """
+
+    if remove_existing:
+        remove_xyz_connections()
+
+    sources = SOURCES + extra_sources
+
+    # Add sources to browser
+    for source in sources:
+        connectionName = source[0]
+        QSettings().setValue(f"qgis/connections-xyz/{connectionName}/authcfg", source[1])
+        QSettings().setValue(f"qgis/connections-xyz/{connectionName}/password", source[2])
+        QSettings().setValue(f"qgis/connections-xyz/{connectionName}/referer", source[3])
+        QSettings().setValue(f"qgis/connections-xyz/{connectionName}/url", source[4])
+        QSettings().setValue(f"qgis/connections-xyz/{connectionName}/username", source[5])
+        QSettings().setValue(f"qgis/connections-xyz/{connectionName}/zmax", source[6])
+        QSettings().setValue(f"qgis/connections-xyz/{connectionName}/zmin", source[7])
+
+    iface.reloadConnections()
---
+++ browser_helper/update_postgis.py
@@ -0,0 +1,78 @@
+"""
+Summary of ``update_postgis.py``
+--------------------------------
+
+Use the database connections defined in postGIS_tools.get_postGIS_config()
+to add DB connections under PostGIS within the QGIS Browser.
+
+"""
+
+import postGIS_tools
+
+
+def remove_all_connections() -> None:
+    """ Remove all entries under PostGIS in the Browser """
+    qs = QSettings()
+
+    for k in sorted(qs.allKeys()):
+        if "PostgreSQL/connections/" in k:
+            qs.remove(k)
+
+    iface.reloadConnections()
+
+
+def sync_postgis_config(
+    dbs_to_ignore: list = ["defaultdb", "_dodb"],
+    remove_existing: bool = True,
+) -> None:
+    """
+    Add a database connection for all DBs that exist on the clusters
+    defined in the user's config.txt
+
+    For more details, see postGIS_tools.get_postGIS_config()
+
+    :param dbs_to_ignore: list of databases to skip creating entries for
+    :param remove_existing: if True, start by removing the user's DB list
+    """
+
+    if remove_existing:
+        remove_all_connections()
+
+    # Read the user's config.txt
+    config, super_config = postGIS_tools.get_postGIS_config()
+
+    # Iterate over each host defined in the file
+    for host in config:
+
+        this_config = config[host]
+
+        # Get a list of all databases on the host host cluster
+        super_uri = postGIS_tools.make_uri(**super_config[host])
+        db_list = postGIS_tools.get_database_list(super_uri, debug=False)
+
+        for db in db_list:
+
+            if db not in dbs_to_ignore:
+
+                # Define the name it will show in the QGIS Browser
+                entry = f"{db} {host.upper()}"
+
+                values = {
+                    "database": db,
+                    "host": this_config["host"],
+                    "port": this_config["port"],
+                    "username": this_config["username"],
+                    "password": this_config["password"],
+                    "projectsInDatabase": "true",
+                    "savePassword": "true",
+                    "saveUsername": "true"
+                }
+
+                if "sslmode" in this_config:
+                    values["sslmode"] = this_config["sslmode"]
+
+                for value_name in values:
+                    val = values[value_name]
+                    QSettings().setValue(f"PostgreSQL/connections/{entry}/{value_name}", val)
+
+    iface.reloadConnections()
